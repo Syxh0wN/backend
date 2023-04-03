b015aa95150082d2de078b40bc6115b49cc90bbf
Syxh0wN <leonardobennr@gmail.com>
2023-04-03 12:56:48 -0300

    feat: structure

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { app } from "./app";
+import { AppDataSource } from "./data-source";
+
+(async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log("DataBase connected!");
+    app.listen(process.env.PORT, () => {
+      console.log(`Servidor executando na porta ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.error("Error during Data Source initialization", err);
+  }
+})();
